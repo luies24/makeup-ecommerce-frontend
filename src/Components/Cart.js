5ce3a2a4b0cce5b745a820027cbef070a85f0e4b
Luis Garcia <l.garcia24@hotmail.com>
2022-02-01 01:23:56 -0600

    fixed most of the styling in cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,20 +4,30 @@ export default function Cart(){
     return(
         <div className='cart-main'>
                 <h1 className='cart-heading'>Your Cart</h1>
+                <div className='section-headings'>
+                    <h3>Product</h3>
+                    <div className='pqt-heading'>
+                        <h3>Price</h3>
+                        <h3>Quantity</h3>
+                        <h3>Total</h3>
+                    </div>
+                </div>
             <div className='item-section'>
-                <img 
-                    className='cart-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
-                    alt='Product images'
-                />
-                <div className='item-name-remove'>
-                    <h1>Product Name</h1>
-                    <button className='remove-btn'>Remove</button>
+                <div className='cart-img-name-remove'>
+                    <img 
+                        className='cart-img'
+                        src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                        alt='Product images'
+                    />
+                    <div className='item-name-remove'>
+                        <h1 className='product-name'>Product Name</h1>
+                        <button className='remove-btn'>Remove</button>
+                    </div>
                 </div>
                 <div className='price-qty-total'>
-                    <h2 className='price'>Price</h2>
-                    <h2>Quantity</h2>
-                    <h2>Total</h2>
+                    <h3 className='price'>$20.00</h3>
+                    <h3>Quantity</h3>
+                    <h3>$20.00</h3>
                 </div>
             </div>
             <div className='checkout-cs-btn'>
