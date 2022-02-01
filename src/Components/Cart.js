06cd07bdd9ab74c49a5ed3099f1092e539b5f83f
Luis Garcia <l.garcia24@hotmail.com>
2022-02-01 00:34:28 -0600

    added styling to cart elements

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -10,11 +10,15 @@ export default function Cart(){
                     src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
                     alt='Product images'
                 />
-                <h1>Product Name</h1>
-                <button>Remove</button>
-                <h2>Price</h2>
-                <h2>Quantity</h2>
-                <h2>Total</h2>
+                <div className='item-name-remove'>
+                    <h1>Product Name</h1>
+                    <button className='remove-btn'>Remove</button>
+                </div>
+                <div className='price-qty-total'>
+                    <h2 className='price'>Price</h2>
+                    <h2>Quantity</h2>
+                    <h2>Total</h2>
+                </div>
             </div>
             <div className='checkout-cs-btn'>
                 <button className='cart-btn'>Continue Shopping</button>
