ad1a7e9d9921f197cef2f2aec206b8fc149e053c
Luis Garcia <l.garcia24@hotmail.com>
2022-02-02 20:15:43 -0600

    added some styling to product detail component

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -4,12 +4,13 @@ export default function ProductDetails(){
     return(
         <div className='product-main'>
             <img 
+                className='pd-img'
                 src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*' 
                 alt='Product' />
-            <div>
-                <h1>Product Name</h1>
-                <h4>Price</h4>
-                <button>Add to Cart</button>
+            <div className='info-section'>
+                <h1 className='pd-name'>Product Name</h1>
+                <p className='pd-price'>$20.00</p>
+                <button className='pd-btn'>Add to Cart</button>
                 <p>Product Description</p>
             </div>
         </div>
