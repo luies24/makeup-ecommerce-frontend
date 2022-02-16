a0749cc85f6e5744ea3bf59d41b5553b239a484e
Luis Garcia <l.garcia24@hotmail.com>
2022-02-16 00:44:10 -0600

    added componentDidMount and routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React from 'react'
 import './App.css'
-import {Route, Switch} from 'react-router-dom'
+import { 
+    Switch,
+    Route
+       } from 'react-router-dom'
+
 import Header from './Components/Header'
 import SignUp from './Components/SignUp'
 import Products from './Components/Products'
 import Cart from './Components/Cart'
 import ProductDetails from './Components/ProductDetails'
 import Checkout from './Components/Checkout'
+import Card from './Components/Card'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      {/* <Products /> */}
-      {/* <ProductDetails /> */}
-      {/* <SignUp /> */}
-      {/* <Cart /> */}
-      <Checkout />
-      {/* <Switch>
-        <Route path='/Products' />
-        <Route path='/SignUp' />
-        <Route path='/Cart' />
-      </Switch> */}
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    inventory: [],
+    selectedProduct: [],
+    favorites: [],
+    cart: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/middleman')
+      .then(response => response.json())
+      .then(inventory => this.setState({ inventory }))
+  }
+  
+  render() {
+    return (
+      <div>
+        {console.log(this.state.inventory)}
+        <Header />
+        <Switch>
+            <Route path='/' render={props => <Products/>} />
+            <Route path='/Card' render={props => <Card />} />
+            <Route path='/ProductDetails' render={props => <ProductDetails />} />
+            <Route path='/SignUp' render={props => <SignUp />} />
+            <Route path='/Cart' render={props => <Cart />} />
+            <Route path='/Checkout' render={props => <Checkout />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,73 +6,73 @@ export default function Products(){
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://cdn.shopify.com/s/files/1/1338/0845/collections/lippie-pencil_grande.jpg?v=1512588769'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$5.00</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://cdn.shopify.com/s/files/1/1338/0845/products/brain-freeze_a_800x1200.jpg?v=1502255076'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$5.50</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://cdn.shopify.com/s/files/1/1338/0845/collections/blottedlip-lippie-stix_grande.jpg?v=1512588803'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$5.50</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://www.purpicks.com/wp-content/uploads/csm/sunset-bronze-pennylaneorganics1.jpg'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$12.00</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
              <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://www.purpicks.com/wp-content/uploads/2018/03/rejuva-minerals-bronzer.jpg'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$26.00</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://www.purpicks.com/wp-content/uploads/2018/03/lotus-cosmetics-usa-mascara-black.png'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$6.70</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://www.purpicks.com/wp-content/uploads/2018/03/lotus-cosmetics-usa-eye-shadow-palette-cocoa.png'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$6.90</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
             <div className='product-card'>
                 <img 
                     className='card-img'
-                    src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1-index-best-makeup-products-1614372277.jpg?crop=0.478xw:0.955xh;0.266xw,0.0256xh&resize=640:*'
+                    src='https://www.purpicks.com/wp-content/uploads/2018/06/Nudus-Amalia.jpg'
                     alt='Makeup Product'    
                 />
-                <h3 className='price'>$20.00</h3>
+                <h3 className='price'>$15.00</h3>
                 <button className='add-to-cart'>Add to Cart</button>
             </div>
         </div>
